d4d0e3d09bde782b445ce7cefb04bed2a5477861
Khuyen Tran <khuyentran1476@gmail.com>
2022-10-24 08:57:17 -0500

    edit pyfakefs

--- Chapter5/test_pyfakefs.py
+++ Chapter5/test_pyfakefs.py
@@ -1,18 +1,21 @@
 from pathlib import Path
 
 
-def create_file(folder, file_name):
-    """Create new file inside the folder"""
+def save_result(folder: str, file_name: str, result: str):
+    # Create new file inside the folder
     file = Path(folder) / file_name
     file.touch()
 
+    # Write result to the new file
+    file.write_text(result)
 
-def test_create_file(fs):
-    """Test if create_file is creating a new file in the fake directory"""
-
+def test_save_result(fs):
     folder = "new"
-    # Create fake directory
+    file_name = "my_file.txt"
+    result = "The accuracy is 0.9"
+
     fs.create_dir(folder)
-    # Test create_file
-    create_file(folder, "my_file.txt")
-    assert Path("new/my_file.txt").exists()
+
+    save_result(folder=folder, file_name=file_name, result=result)
+    res = Path(f"{folder}/{file_name}").read_text()
+    assert res == result
