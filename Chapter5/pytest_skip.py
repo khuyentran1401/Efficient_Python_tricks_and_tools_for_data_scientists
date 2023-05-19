38d3a7e264a48b36657234edc2000a538b8c227f
khuyentran1401 <khuyentran1476@gmail.com>
2023-05-19 08:58:49 -0500

    clean up Chapter5 dir

--- Chapter5/pytest_skip.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pytest 
-
-def add_two(num: int):
-    return num + 2 
-
-@pytest.mark.skipif(sys.version_info < (3, 9), reason="Eequires Python 3.9 or higher")
-def test_add_two(): 
-    assert add_two(3) == 5
--- Chapter5/streamlit_app.py
+++
@@ -1,6 +0,0 @@
-import spacy_streamlit
-
-models = ["en_core_web_sm"]
-text = "Today is a beautiful day"
-spacy_streamlit.visualize(models, text)
-
