64f070707b0355d6de22e07cb775fcb9776b8d62
khuyentran1401 <khuyentran1476@gmail.com>
2025-02-25 16:33:54 +0700

    add content

--- Chapter6/main.py
+++ Chapter6/main.py
@@ -1,3 +1,9 @@
-from utils import get_mean  
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
-get_mean(1, 3)
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def my_app(cfg: DictConfig) -> None:
+    print(OmegaConf.to_yaml(cfg))
+
+if __name__ == "__main__":
+    my_app()
